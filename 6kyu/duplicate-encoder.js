e1b320e03beeb8389dd370c8a4bb86c518bf756e
max_mr <mvnikitin.job@gmail.com>
2025-06-08 12:34:16 +0300

    duplicate encoder

--- 6kyu/duplicate-encoder.js
+++ 6kyu/duplicate-encoder.js
@@ -7,10 +7,7 @@
 // "recede"   =>  "()()()"
 // "Success"  =>  ")())())"
 // "(( @"     =>  "))(("
-//
-// Notes
-//
-// Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!
+
 
 // my first version
 // function duplicateEncode(word){
