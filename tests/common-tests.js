0b694d673ad8f6b13bf5818310d8545a68d2639d
mvnikitin <mvnikitin.job@gmail.com>
2025-06-27 13:48:28 +0300

    count-characters-in-your-string

--- tests/common-tests.js
+++ tests/common-tests.js
@@ -1,37 +1,52 @@
-const { assert } = require("chai")
-const { solution } = require("../6kyu/multiplies-of3or5.js")
-const { duplicateCount} = require("../6kyu/counting-duplicates.js")
+const { assert } = require("chai");
+const { solution } = require("../6kyu/multiplies-of3or5.js");
+const { duplicateCount } = require("../6kyu/counting-duplicates.js");
+const { count } = require("../6kyu/count-characters-in-your-string.js");
 
-function test(n, expected, func) {
-    it(`n=${n}`, () => {
-        let actual = func(n)
-        assert.strictEqual(actual, expected)
-    })
+function testStrict(n, expected, func) {
+  it(`n=${n}`, () => {
+    let actual = func(n);
+    assert.strictEqual(actual, expected);
+  });
+}
+
+function testDeep(n, expected, func) {
+  it(`n=${n}`, () => {
+    let actual = func(n);
+    assert.deepEqual(actual, expected);
+  })
 }
 
 describe("counting duplicates", () => {
-    test("abcde", 0, duplicateCount)
-    test("aabbcde", 2, duplicateCount)
-    test("aabBcde", 2, duplicateCount)
-    test("indivisibility", 1, duplicateCount)
-    test("Indivisibilities", 2, duplicateCount)
-    test("aA11", 2, duplicateCount)
-    test("ABBA", 2, duplicateCount)
-    test("", 0, duplicateCount)
-    test("abcc cdefccghijklc", 1, duplicateCount)
-})
+  testStrict("abcde", 0, duplicateCount);
+  testStrict("aabbcde", 2, duplicateCount);
+  testStrict("aabBcde", 2, duplicateCount);
+  testStrict("indivisibility", 1, duplicateCount);
+  testStrict("Indivisibilities", 2, duplicateCount);
+  testStrict("aA11", 2, duplicateCount);
+  testStrict("ABBA", 2, duplicateCount);
+  testStrict("", 0, duplicateCount);
+  testStrict("abcc cdefccghijklc", 1, duplicateCount);
+});
 
-describe("multiplies of 3 or 5", function(){
-    test(-1, 0, solution)
-    test(0, 0, solution)
-    test(1, 0, solution)
-    test(2, 0, solution)
-    test(3, 0, solution)
-    test(4, 3, solution)
-    test(5, 3, solution)
-    test(6, 8, solution)
-    test(7, 14, solution)
-    test(8, 14, solution)
-    test(9, 14, solution)
-    test(10,23, solution)
-})
+describe("multiplies of 3 or 5", function () {
+  testStrict(-1, 0, solution);
+  testStrict(0, 0, solution);
+  testStrict(1, 0, solution);
+  testStrict(2, 0, solution);
+  testStrict(3, 0, solution);
+  testStrict(4, 3, solution);
+  testStrict(5, 3, solution);
+  testStrict(6, 8, solution);
+  testStrict(7, 14, solution);
+  testStrict(8, 14, solution);
+  testStrict(9, 14, solution);
+  testStrict(10, 23, solution);
+});
+
+describe("count characters in string", function () {
+  testDeep("habahaba", { h: 2, a: 4, b: 2 }, count);
+  testDeep('', {}, count)
+  testDeep('ABCd', {A: 1, B: 1, C: 1, d: 1}, count)
+  testDeep('abrAcadaBra', {a: 4, b: 1, r: 2, A: 1, c: 1, d: 1, B: 1}, count)
+});
